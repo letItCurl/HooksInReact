39ce7149e9fda195ee309497623db9375b3e6407
rollpz <rolandlopez.developer@gmail.com>
2020-03-17 19:29:16 +0100

    first training

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [number, setNumber] = useState(10)
+ 
+  const increase = () => setNumber(number + 1)
+  const decrease = () => setNumber(number - 1)
+  const setItOff = () => setNumber(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button className="button is-dark" onClick={increase} >Increase</button>
+      <button className="button is-dark" onClick={decrease} >Decrease</button>
+      <button className="button is-dark" onClick={setItOff} >Set it off</button>
+      <p>{number}</p>
     </div>
   );
 }
